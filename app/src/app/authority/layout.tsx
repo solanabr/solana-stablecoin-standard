99339dbb74cf35947dea054a082ca2c12ee77e17
amanhij <amanjemal71@gmail.com>
2026-03-13 20:54:53 +0800

    Fix authority layout function name

--- app/src/app/authority/layout.tsx
+++ app/src/app/authority/layout.tsx
@@ -13,7 +13,7 @@ const jetbrainsMono = JetBrains_Mono({
   weight: ["400", "500", "700"],
 });
 
-export default function AllowlistLayout({
+export default function AuthorityLayout({
   children,
 }: {
   children: React.ReactNode;
